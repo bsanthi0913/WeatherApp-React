cb37be7921dda28f963ca1b078ff577b78f5c942
tlokesh@enphaeenergy.com <tlokesh@IN-TLOKESH-MBP.local>
2023-09-14 16:06:22 +0530

    added hourly temp and time forecast

--- v1/src/components/WeatherBlocks.jsx
+++ v1/src/components/WeatherBlocks.jsx
@@ -65,8 +65,98 @@ export default function WeatherBlocks(props) {
     if (minutes.length == 1) {
       minutes = "0" + minutes;
     }
-    Number(hours);
+    hours = Number(hours);
     return hours;
+  }
+  function getHours1(datetimeEpoch, datetime) {
+    const date = new Date(datetimeEpoch * 1000);
+    // Get the day of the week (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
+    const dayOfWeek = date.toLocaleDateString("en-US", { weekday: "long" });
+    // Get the time in 12-hour format (e.g., 02:30 PM)
+    const timeString = datetime; // Replace with your time string
+    // Split the time string into hours and minutes
+    let [hours, minutes] = timeString.split(":").map(String);
+    if (minutes.length == 1) {
+      minutes = "0" + minutes;
+    }
+    hours = Number(hours);
+    // alert(typeof hours);
+    return hours + 1;
+  }
+  function getHours2(datetimeEpoch, datetime) {
+    const date = new Date(datetimeEpoch * 1000);
+    // Get the day of the week (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
+    const dayOfWeek = date.toLocaleDateString("en-US", { weekday: "long" });
+    // Get the time in 12-hour format (e.g., 02:30 PM)
+    const timeString = datetime; // Replace with your time string
+    // Split the time string into hours and minutes
+    let [hours, minutes] = timeString.split(":").map(String);
+    if (minutes.length == 1) {
+      minutes = "0" + minutes;
+    }
+    hours = Number(hours);
+    // alert(typeof hours);
+    return hours + 2;
+  }
+  function getHours3(datetimeEpoch, datetime) {
+    const date = new Date(datetimeEpoch * 1000);
+    // Get the day of the week (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
+    const dayOfWeek = date.toLocaleDateString("en-US", { weekday: "long" });
+    // Get the time in 12-hour format (e.g., 02:30 PM)
+    const timeString = datetime; // Replace with your time string
+    // Split the time string into hours and minutes
+    let [hours, minutes] = timeString.split(":").map(String);
+    if (minutes.length == 1) {
+      minutes = "0" + minutes;
+    }
+    hours = Number(hours);
+    // alert(typeof hours);
+    return hours + 3;
+  }
+  function getHours4(datetimeEpoch, datetime) {
+    const date = new Date(datetimeEpoch * 1000);
+    // Get the day of the week (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
+    const dayOfWeek = date.toLocaleDateString("en-US", { weekday: "long" });
+    // Get the time in 12-hour format (e.g., 02:30 PM)
+    const timeString = datetime; // Replace with your time string
+    // Split the time string into hours and minutes
+    let [hours, minutes] = timeString.split(":").map(String);
+    if (minutes.length == 1) {
+      minutes = "0" + minutes;
+    }
+    hours = Number(hours);
+    // alert(typeof hours);
+    return hours + 4;
+  }
+  function getHours5(datetimeEpoch, datetime) {
+    const date = new Date(datetimeEpoch * 1000);
+    // Get the day of the week (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
+    const dayOfWeek = date.toLocaleDateString("en-US", { weekday: "long" });
+    // Get the time in 12-hour format (e.g., 02:30 PM)
+    const timeString = datetime; // Replace with your time string
+    // Split the time string into hours and minutes
+    let [hours, minutes] = timeString.split(":").map(String);
+    if (minutes.length == 1) {
+      minutes = "0" + minutes;
+    }
+    hours = Number(hours);
+    // alert(typeof hours);
+    return hours + 5;
+  }
+  function getHours6(datetimeEpoch, datetime) {
+    const date = new Date(datetimeEpoch * 1000);
+    // Get the day of the week (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
+    const dayOfWeek = date.toLocaleDateString("en-US", { weekday: "long" });
+    // Get the time in 12-hour format (e.g., 02:30 PM)
+    const timeString = datetime; // Replace with your time string
+    // Split the time string into hours and minutes
+    let [hours, minutes] = timeString.split(":").map(String);
+    if (minutes.length == 1) {
+      minutes = "0" + minutes;
+    }
+    hours = Number(hours);
+    // alert(typeof hours);
+    return hours + 6;
   }
   function getDayTime(datetimeEpoch, datetime) {
     const date = new Date(datetimeEpoch * 1000);
@@ -93,6 +183,85 @@ export default function WeatherBlocks(props) {
       return `${hours12 + 1} ${pmam}`;
     }
     if (hours12 == 12) {
+      return `${hours12 - 11} ${ampm}`;
+    }
+    return `${hours12 + 1} ${ampm}`;
+  }
+  function getDayTime1(datetimeEpoch, datetime) {
+    const t = getDayTime(datetimeEpoch, datetime);
+
+    const hours12 = Number(t.slice(-5, -3));
+    const ampm = t.slice(-2);
+    if (hours12 === 11 && ampm === "AM") {
+      return `${hours12 + 1} PM`;
+    }
+    if (hours12 === 11 && ampm === "PM") {
+      return `${hours12 + 1} AM`;
+    }
+    if (hours12 === 12) {
+      return `${hours12 - 11} ${ampm}`;
+    }
+    // alert(ampm);
+    return `${hours12 + 1} ${ampm}`;
+  }
+  function getDayTime2(datetimeEpoch, datetime) {
+    const t = getDayTime1(datetimeEpoch, datetime);
+    const hours12 = Number(t.slice(-5, -3));
+
+    const ampm = t.slice(-2);
+    if (hours12 === 11 && ampm === "AM") {
+      return `${hours12 + 1} PM`;
+    }
+    if (hours12 === 11 && ampm === "PM") {
+      return `${hours12 + 1} AM`;
+    }
+    if (hours12 === 12) {
+      return `${hours12 - 11} ${ampm}`;
+    }
+
+    return `${hours12 + 1} ${ampm}`;
+  }
+  function getDayTime3(datetimeEpoch, datetime) {
+    const t = getDayTime2(datetimeEpoch, datetime);
+    const hours12 = Number(t.slice(-5, -3));
+    const ampm = t.slice(-2);
+    if (hours12 === 11 && ampm === "AM") {
+      return `${hours12 + 1} PM`;
+    }
+    if (hours12 === 11 && ampm === "PM") {
+      return `${hours12 + 1} AM`;
+    }
+    if (hours12 === 12) {
+      return `${hours12 - 11} ${ampm}`;
+    }
+    return `${hours12 + 1} ${ampm}`;
+  }
+  function getDayTime4(datetimeEpoch, datetime) {
+    const t = getDayTime3(datetimeEpoch, datetime);
+    const hours12 = Number(t.slice(-5, -3));
+    const ampm = t.slice(-2);
+    if (hours12 === 11 && ampm === "AM") {
+      return `${hours12 + 1} PM`;
+    }
+    if (hours12 === 11 && ampm === "PM") {
+      return `${hours12 + 1} AM`;
+    }
+    if (hours12 === 12) {
+      return `${hours12 - 11} ${ampm}`;
+    }
+    return `${hours12 + 1} ${ampm}`;
+  }
+  function getDayTime5(datetimeEpoch, datetime) {
+    const t = getDayTime4(datetimeEpoch, datetime);
+    const hours12 = Number(t.slice(-5, -3));
+    const ampm = t.slice(-2);
+    if (hours12 === 11 && ampm === "AM") {
+      return `${hours12 + 1} PM`;
+    }
+    if (hours12 === 11 && ampm === "PM") {
+      return `${hours12 + 1} AM`;
+    }
+    if (hours12 === 12) {
       return `${hours12 - 11} ${ampm}`;
     }
     return `${hours12 + 1} ${ampm}`;
@@ -207,28 +376,191 @@ export default function WeatherBlocks(props) {
       );
     }
   }, [props.data]);
-  // useEffect(() => {
-  //   if (props.data && props.data.days) {
-  //     if (
-  //       getHours(
-  //         props.data.currentConditions.datetimeEpoch,
-  //         props.data.currentConditions.datetime
-  //       ) === 23
-  //     )
-  //       hsetTemp1(FtoC(props.data.days[1].hours[0].temp));
-  //     else
-  //       hsetTemp1(
-  //         FtoC(
-  //           props.data.days[0].hours[
-  //             getHours(
-  //               props.data.currentConditions.datetimeEpoch,
-  //               props.data.currentConditions.datetime
-  //             ) + 1
-  //           ].temp
-  //         )
-  //       );
-  //   }
-  // }, [props.data]);
+  useEffect(() => {
+    if (props.data && props.data.days) {
+      hsetTime2(
+        getDayTime1(
+          props.data.currentConditions.datetimeEpoch,
+          props.data.currentConditions.datetime
+        )
+      );
+    }
+  }, [props.data]);
+  useEffect(() => {
+    if (props.data && props.data.days) {
+      hsetTime3(
+        getDayTime2(
+          props.data.currentConditions.datetimeEpoch,
+          props.data.currentConditions.datetime
+        )
+      );
+    }
+  }, [props.data]);
+  useEffect(() => {
+    if (props.data && props.data.days) {
+      hsetTime4(
+        getDayTime3(
+          props.data.currentConditions.datetimeEpoch,
+          props.data.currentConditions.datetime
+        )
+      );
+    }
+  }, [props.data]);
+  useEffect(() => {
+    if (props.data && props.data.days) {
+      hsetTime5(
+        getDayTime4(
+          props.data.currentConditions.datetimeEpoch,
+          props.data.currentConditions.datetime
+        )
+      );
+    }
+  }, [props.data]);
+  useEffect(() => {
+    if (props.data && props.data.days) {
+      hsetTime6(
+        getDayTime5(
+          props.data.currentConditions.datetimeEpoch,
+          props.data.currentConditions.datetime
+        )
+      );
+    }
+  }, [props.data]);
+
+  // temp
+
+  useEffect(() => {
+    if (props.data && props.data.days) {
+      if (
+        getHours(
+          props.data.currentConditions.datetimeEpoch,
+          props.data.currentConditions.datetime
+        ) === 23
+      )
+        hsetTemp1(FtoC(props.data.days[1].hours[0].temp));
+      else
+        hsetTemp1(
+          FtoC(
+            props.data.days[0].hours[
+              getHours1(
+                props.data.currentConditions.datetimeEpoch,
+                props.data.currentConditions.datetime
+              )
+            ].temp
+          )
+        );
+    }
+  }, [props.data]);
+  useEffect(() => {
+    if (props.data && props.data.days) {
+      if (
+        getHours(
+          props.data.currentConditions.datetimeEpoch,
+          props.data.currentConditions.datetime
+        ) === 22
+      )
+        hsetTemp2(FtoC(props.data.days[1].hours[0].temp));
+      else
+        hsetTemp2(
+          FtoC(
+            props.data.days[0].hours[
+              getHours2(
+                props.data.currentConditions.datetimeEpoch,
+                props.data.currentConditions.datetime
+              )
+            ].temp
+          )
+        );
+    }
+  }, [props.data]);
+  useEffect(() => {
+    if (props.data && props.data.days) {
+      if (
+        getHours(
+          props.data.currentConditions.datetimeEpoch,
+          props.data.currentConditions.datetime
+        ) === 21
+      )
+        hsetTemp3(FtoC(props.data.days[1].hours[0].temp));
+      else
+        hsetTemp3(
+          FtoC(
+            props.data.days[0].hours[
+              getHours3(
+                props.data.currentConditions.datetimeEpoch,
+                props.data.currentConditions.datetime
+              )
+            ].temp
+          )
+        );
+    }
+  }, [props.data]);
+  useEffect(() => {
+    if (props.data && props.data.days) {
+      if (
+        getHours(
+          props.data.currentConditions.datetimeEpoch,
+          props.data.currentConditions.datetime
+        ) === 20
+      )
+        hsetTemp4(FtoC(props.data.days[1].hours[0].temp));
+      else
+        hsetTemp4(
+          FtoC(
+            props.data.days[0].hours[
+              getHours4(
+                props.data.currentConditions.datetimeEpoch,
+                props.data.currentConditions.datetime
+              )
+            ].temp
+          )
+        );
+    }
+  }, [props.data]);
+  useEffect(() => {
+    if (props.data && props.data.days) {
+      if (
+        getHours(
+          props.data.currentConditions.datetimeEpoch,
+          props.data.currentConditions.datetime
+        ) === 19
+      )
+        hsetTemp5(FtoC(props.data.days[1].hours[0].temp));
+      else
+        hsetTemp5(
+          FtoC(
+            props.data.days[0].hours[
+              getHours5(
+                props.data.currentConditions.datetimeEpoch,
+                props.data.currentConditions.datetime
+              )
+            ].temp
+          )
+        );
+    }
+  }, [props.data]);
+  useEffect(() => {
+    if (props.data && props.data.days) {
+      if (
+        getHours(
+          props.data.currentConditions.datetimeEpoch,
+          props.data.currentConditions.datetime
+        ) === 18
+      )
+        hsetTemp6(FtoC(props.data.days[1].hours[0].temp));
+      else
+        hsetTemp6(
+          FtoC(
+            props.data.days[0].hours[
+              getHours6(
+                props.data.currentConditions.datetimeEpoch,
+                props.data.currentConditions.datetime
+              )
+            ].temp
+          )
+        );
+    }
+  }, [props.data]);
   if (toggle) {
     return (
       <div className="main">
@@ -319,57 +651,57 @@ export default function WeatherBlocks(props) {
               <img src={icon1} alt="no-image" />
             </div>
             <div className="day-temp">
-              <h2 className="temp">{temp1}</h2>
+              <h2 className="temp">{htemp1}</h2>
               <span className="temp-unit"> °C</span>
             </div>
           </li>
           <li className="card">
-            <h2 className="day-name">{time2}</h2>
+            <h2 className="day-name">{htime2}</h2>
             <div className="card-icon">
               <img src={icon2} alt="no-image" />
             </div>
             <div className="day-temp">
-              <h2 className="temp">{temp2}</h2>
+              <h2 className="temp">{htemp2}</h2>
               <span className="temp-unit"> °C</span>
             </div>
           </li>
           <li className="card">
-            <h2 className="day-name">{time3}</h2>
+            <h2 className="day-name">{htime3}</h2>
             <div className="card-icon">
               <img src={icon3} alt="no-image" />
             </div>
             <div className="day-temp">
-              <h2 className="temp">{temp3}</h2>
+              <h2 className="temp">{htemp3}</h2>
               <span className="temp-unit"> °C</span>
             </div>
           </li>
           <li className="card">
-            <h2 className="day-name">{time4}</h2>
+            <h2 className="day-name">{htime4}</h2>
             <div className="card-icon">
               <img src={icon4} alt="no-image" />
             </div>
             <div className="day-temp">
-              <h2 className="temp">{temp4}</h2>
+              <h2 className="temp">{htemp4}</h2>
               <span className="temp-unit"> °C</span>
             </div>
           </li>
           <li className="card">
-            <h2 className="day-name">{time5}</h2>
+            <h2 className="day-name">{htime5}</h2>
             <div className="card-icon">
               <img src={icon5} alt="no-image" />
             </div>
             <div className="day-temp">
-              <h2 className="temp">{temp5}</h2>
+              <h2 className="temp">{htemp5}</h2>
               <span className="temp-unit"> °C</span>
             </div>
           </li>
           <li className="card">
-            <h2 className="day-name">{time6}</h2>
+            <h2 className="day-name">{htime6}</h2>
             <div className="card-icon">
               <img src={icon6} alt="no-image" />
             </div>
             <div className="day-temp">
-              <h2 className="temp">{temp6}</h2>
+              <h2 className="temp">{htemp6}</h2>
               <span className="temp-unit"> °C</span>
             </div>
           </li>
