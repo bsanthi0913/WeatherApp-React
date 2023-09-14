bdcab053450746afde91a023c35b26c736de849a
tlokesh@enphaeenergy.com <tlokesh@IN-TLOKESH-MBP.local>
2023-09-14 12:37:50 +0530

    added hourly time

--- v1/src/components/WeatherBlocks.jsx
+++ v1/src/components/WeatherBlocks.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 export default function WeatherBlocks(props) {
+  const [toggle, setToggle] = useState(true);
   const [time1, setTime1] = useState("Tuesday");
   const [time2, setTime2] = useState("Wednesday");
   const [time3, setTime3] = useState("Thursday");
@@ -19,7 +20,29 @@ export default function WeatherBlocks(props) {
   const [temp4, setTemp4] = useState("32");
   const [temp5, setTemp5] = useState("32");
   const [temp6, setTemp6] = useState("32");
+  // hourly
+  const [htime1, hsetTime1] = useState("6:00 AM");
+  const [htime2, hsetTime2] = useState("7:00 AM");
+  const [htime3, hsetTime3] = useState("8:00 AM");
+  const [htime4, hsetTime4] = useState("9:00 AM");
+  const [htime5, hsetTime5] = useState("10:00 AM");
+  const [htime6, hsetTime6] = useState("11:00 AM");
+  const [hicon1, hsetIcon1] = useState("./weather-icons/clear-day.svg");
+  const [hicon2, hsetIcon2] = useState("./weather-icons/clear-day.svg");
+  const [hicon3, hsetIcon3] = useState("./weather-icons/clear-day.svg");
+  const [hicon4, hsetIcon4] = useState("./weather-icons/clear-day.svg");
+  const [hicon5, hsetIcon5] = useState("./weather-icons/clear-day.svg");
+  const [hicon6, hsetIcon6] = useState("./weather-icons/clear-day.svg");
+  const [htemp1, hsetTemp1] = useState("32");
+  const [htemp2, hsetTemp2] = useState("32");
+  const [htemp3, hsetTemp3] = useState("32");
+  const [htemp4, hsetTemp4] = useState("32");
+  const [htemp5, hsetTemp5] = useState("32");
+  const [htemp6, hsetTemp6] = useState("32");
 
+  const handleClick = () => {
+    setToggle(!toggle);
+  };
   function FtoC(temperature) {
     const F = temperature;
     const C = (5 / 9) * (F - 32);
@@ -31,7 +54,51 @@ export default function WeatherBlocks(props) {
       weekday: "long",
     });
   }
+  function getHours(datetimeEpoch, datetime) {
+    const date = new Date(datetimeEpoch * 1000);
+    // Get the day of the week (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
+    const dayOfWeek = date.toLocaleDateString("en-US", { weekday: "long" });
+    // Get the time in 12-hour format (e.g., 02:30 PM)
+    const timeString = datetime; // Replace with your time string
+    // Split the time string into hours and minutes
+    let [hours, minutes] = timeString.split(":").map(String);
+    if (minutes.length == 1) {
+      minutes = "0" + minutes;
+    }
+    Number(hours);
+    return hours;
+  }
+  function getDayTime(datetimeEpoch, datetime) {
+    const date = new Date(datetimeEpoch * 1000);
+    // Get the day of the week (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
+    const dayOfWeek = date.toLocaleDateString("en-US", { weekday: "long" });
+    // Get the time in 12-hour format (e.g., 02:30 PM)
+    const timeString = datetime; // Replace with your time string
+    // Split the time string into hours and minutes
+    let [hours, minutes] = timeString.split(":").map(String);
+    if (minutes.length == 1) {
+      minutes = "0" + minutes;
+    }
+    Number(hours);
+    // Determine whether it's AM or PM
+    const ampm = hours >= 12 ? "PM" : "AM";
+    const pmam = hours >= 12 ? "AM" : "PM";
+    // Convert to 12-hour format
+    const hours12 = hours % 12 || 12;
 
+    // Create the formatted time string
+
+    const formattedTime = `${hours12}:${minutes} ${ampm}`;
+    if (hours12 === 11) {
+      return `${hours12 + 1} ${pmam}`;
+    }
+    if (hours12 == 12) {
+      return `${hours12 - 11} ${ampm}`;
+    }
+    return `${hours12 + 1} ${ampm}`;
+  }
+
+  // daily
   // time
   useEffect(() => {
     if (props.data && props.data.days) {
@@ -127,77 +194,187 @@ export default function WeatherBlocks(props) {
       setTemp6(FtoC(props.data.days[6].temp));
     }
   }, [props.data]);
-  return (
-    <div className="main">
-      {/* <h1 className="weather-condition">Rainfall</h1> */}
-      <nav>
-        <ul className="options">
-          <button className="hourly">Today</button>
-          <button className="weekly">This Week</button>
+  // hourly
+
+  //time
+  useEffect(() => {
+    if (props.data && props.data.days) {
+      hsetTime1(
+        getDayTime(
+          props.data.currentConditions.datetimeEpoch,
+          props.data.currentConditions.datetime
+        )
+      );
+    }
+  }, [props.data]);
+  // useEffect(() => {
+  //   if (props.data && props.data.days) {
+  //     if (
+  //       getHours(
+  //         props.data.currentConditions.datetimeEpoch,
+  //         props.data.currentConditions.datetime
+  //       ) === 23
+  //     )
+  //       hsetTemp1(FtoC(props.data.days[1].hours[0].temp));
+  //     else
+  //       hsetTemp1(
+  //         FtoC(
+  //           props.data.days[0].hours[
+  //             getHours(
+  //               props.data.currentConditions.datetimeEpoch,
+  //               props.data.currentConditions.datetime
+  //             ) + 1
+  //           ].temp
+  //         )
+  //       );
+  //   }
+  // }, [props.data]);
+  if (toggle) {
+    return (
+      <div className="main">
+        <button onClick={handleClick} class="btn btn-dark mb-5">
+          Toggle State
+        </button>
+        {/* <h1 className="weather-condition">Rainfall</h1> */}
+        {/* <nav>
+          <ul className="options">
+            <button className="hourly">Today</button>
+            <button className="weekly">This Week</button>
+          </ul>
+        </nav> */}
+        <ul className="cards">
+          <li className="card">
+            <h2 className="day-name">{time1}</h2>
+            <div className="card-icon">
+              <img src={icon1} alt="no-image" />
+            </div>
+            <div className="day-temp">
+              <h2 className="temp">{temp1}</h2>
+              <span className="temp-unit"> °C</span>
+            </div>
+          </li>
+          <li className="card">
+            <h2 className="day-name">{time2}</h2>
+            <div className="card-icon">
+              <img src={icon2} alt="no-image" />
+            </div>
+            <div className="day-temp">
+              <h2 className="temp">{temp2}</h2>
+              <span className="temp-unit"> °C</span>
+            </div>
+          </li>
+          <li className="card">
+            <h2 className="day-name">{time3}</h2>
+            <div className="card-icon">
+              <img src={icon3} alt="no-image" />
+            </div>
+            <div className="day-temp">
+              <h2 className="temp">{temp3}</h2>
+              <span className="temp-unit"> °C</span>
+            </div>
+          </li>
+          <li className="card">
+            <h2 className="day-name">{time4}</h2>
+            <div className="card-icon">
+              <img src={icon4} alt="no-image" />
+            </div>
+            <div className="day-temp">
+              <h2 className="temp">{temp4}</h2>
+              <span className="temp-unit"> °C</span>
+            </div>
+          </li>
+          <li className="card">
+            <h2 className="day-name">{time5}</h2>
+            <div className="card-icon">
+              <img src={icon5} alt="no-image" />
+            </div>
+            <div className="day-temp">
+              <h2 className="temp">{temp5}</h2>
+              <span className="temp-unit"> °C</span>
+            </div>
+          </li>
+          <li className="card">
+            <h2 className="day-name">{time6}</h2>
+            <div className="card-icon">
+              <img src={icon6} alt="no-image" />
+            </div>
+            <div className="day-temp">
+              <h2 className="temp">{temp6}</h2>
+              <span className="temp-unit"> °C</span>
+            </div>
+          </li>
         </ul>
-      </nav>
-      <ul className="cards">
-        <li className="card">
-          <h2 className="day-name">{time1}</h2>
-          <div className="card-icon">
-            <img src={icon1} alt="no-image" />
-          </div>
-          <div className="day-temp">
-            <h2 className="temp">{temp1}</h2>
-            <span className="temp-unit"> °C</span>
-          </div>
-        </li>
-        <li className="card">
-          <h2 className="day-name">{time2}</h2>
-          <div className="card-icon">
-            <img src={icon2} alt="no-image" />
-          </div>
-          <div className="day-temp">
-            <h2 className="temp">{temp2}</h2>
-            <span className="temp-unit"> °C</span>
-          </div>
-        </li>
-        <li className="card">
-          <h2 className="day-name">{time3}</h2>
-          <div className="card-icon">
-            <img src={icon3} alt="no-image" />
-          </div>
-          <div className="day-temp">
-            <h2 className="temp">{temp3}</h2>
-            <span className="temp-unit"> °C</span>
-          </div>
-        </li>
-        <li className="card">
-          <h2 className="day-name">{time4}</h2>
-          <div className="card-icon">
-            <img src={icon4} alt="no-image" />
-          </div>
-          <div className="day-temp">
-            <h2 className="temp">{temp4}</h2>
-            <span className="temp-unit"> °C</span>
-          </div>
-        </li>
-        <li className="card">
-          <h2 className="day-name">{time5}</h2>
-          <div className="card-icon">
-            <img src={icon5} alt="no-image" />
-          </div>
-          <div className="day-temp">
-            <h2 className="temp">{temp5}</h2>
-            <span className="temp-unit"> °C</span>
-          </div>
-        </li>
-        <li className="card">
-          <h2 className="day-name">{time6}</h2>
-          <div className="card-icon">
-            <img src={icon6} alt="no-image" />
-          </div>
-          <div className="day-temp">
-            <h2 className="temp">{temp6}</h2>
-            <span className="temp-unit"> °C</span>
-          </div>
-        </li>
-      </ul>
-    </div>
-  );
+      </div>
+    );
+  } else {
+    return (
+      <div className="main">
+        <button onClick={handleClick} class="btn btn-dark mb-5">
+          Toggle State
+        </button>
+        <ul className="cards">
+          <li className="card">
+            <h2 className="day-name">{htime1}</h2>
+            <div className="card-icon">
+              <img src={icon1} alt="no-image" />
+            </div>
+            <div className="day-temp">
+              <h2 className="temp">{temp1}</h2>
+              <span className="temp-unit"> °C</span>
+            </div>
+          </li>
+          <li className="card">
+            <h2 className="day-name">{time2}</h2>
+            <div className="card-icon">
+              <img src={icon2} alt="no-image" />
+            </div>
+            <div className="day-temp">
+              <h2 className="temp">{temp2}</h2>
+              <span className="temp-unit"> °C</span>
+            </div>
+          </li>
+          <li className="card">
+            <h2 className="day-name">{time3}</h2>
+            <div className="card-icon">
+              <img src={icon3} alt="no-image" />
+            </div>
+            <div className="day-temp">
+              <h2 className="temp">{temp3}</h2>
+              <span className="temp-unit"> °C</span>
+            </div>
+          </li>
+          <li className="card">
+            <h2 className="day-name">{time4}</h2>
+            <div className="card-icon">
+              <img src={icon4} alt="no-image" />
+            </div>
+            <div className="day-temp">
+              <h2 className="temp">{temp4}</h2>
+              <span className="temp-unit"> °C</span>
+            </div>
+          </li>
+          <li className="card">
+            <h2 className="day-name">{time5}</h2>
+            <div className="card-icon">
+              <img src={icon5} alt="no-image" />
+            </div>
+            <div className="day-temp">
+              <h2 className="temp">{temp5}</h2>
+              <span className="temp-unit"> °C</span>
+            </div>
+          </li>
+          <li className="card">
+            <h2 className="day-name">{time6}</h2>
+            <div className="card-icon">
+              <img src={icon6} alt="no-image" />
+            </div>
+            <div className="day-temp">
+              <h2 className="temp">{temp6}</h2>
+              <span className="temp-unit"> °C</span>
+            </div>
+          </li>
+        </ul>
+      </div>
+    );
+  }
 }
